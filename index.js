92b6eeb638e30572c86f35e8af9a61bc5ab0ea69
Nabil Freeman <nabil@instafork.com>
2016-07-06 20:42:44 +0800

    staging and debug

--- index.js
+++ index.js
@@ -15,6 +15,22 @@ module.exports = {
         }
     },
 
+    staging: function(callback){
+        if(process.env.NODE_ENV === "staging"){
+            callback();
+        } else if(process.env.NODE_ENV === undefined){
+            console.log("NOTE: freeman-env can't find any value for NODE_ENV.");
+        }
+    },
+
+    debug: function(callback){
+        if(process.env.NODE_ENV === "development" || process.env.NODE_ENV === "staging"){
+            callback();
+        } else if(process.env.NODE_ENV === undefined){
+            console.log("NOTE: freeman-env can't find any value for NODE_ENV.");
+        }
+    },
+
     production: function(callback){
         if(process.env.NODE_ENV === "production"){
             callback();
