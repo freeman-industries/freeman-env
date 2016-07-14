c67180d5302812b26d479ece30cb0f5aa547ff13
Nabil Freeman <nabil@instafork.com>
2016-07-14 11:19:46 +0800

    new syntax (breaking changes)

--- index.js
+++ index.js
@@ -1,41 +1,17 @@
 //freeman-env
 //usage:
 //var env = require('freeman-env');
-//env.production(function(){ //production only code here });
-module.exports = {
-    get: function(){
+//env('production', function(){ //production only code here });
+//env() //returns current NODE_ENV
+module.exports = function(desired_environment, callback){
+    if(!desired_environment && !callback){
+        //get node_env
         return process.env.NODE_ENV;
-    },
-
-    development: function(callback){
-        if(process.env.NODE_ENV === "development"){
-            callback();
-        } else if(process.env.NODE_ENV === undefined){
-            console.log("NOTE: freeman-env can't find any value for NODE_ENV.");
-        }
-    },
-
-    staging: function(callback){
-        if(process.env.NODE_ENV === "staging"){
-            callback();
-        } else if(process.env.NODE_ENV === undefined){
-            console.log("NOTE: freeman-env can't find any value for NODE_ENV.");
-        }
-    },
-
-    debug: function(callback){
-        if(process.env.NODE_ENV === "development" || process.env.NODE_ENV === "staging"){
-            callback();
-        } else if(process.env.NODE_ENV === undefined){
-            console.log("NOTE: freeman-env can't find any value for NODE_ENV.");
-        }
-    },
-
-    production: function(callback){
-        if(process.env.NODE_ENV === "production"){
-            callback();
-        } else if(process.env.NODE_ENV === undefined){
-            console.log("NOTE: freeman-env can't find any value for NODE_ENV.");
-        }
+    } else if(desired_environment && !callback || !desired_environment && callback){
+        throw new Error("freeman-env: Please supply both arguments.");
+        return;
+    } else if(process.env.NODE_ENV === desired_environment) {
+        callback();
+        return;
     }
 }
